b084870e89795542562046d7167607371c002f1f
IetsHD <stefanvd.berg@gmail.com>
2026-05-10 01:39:06 +0200

    ?

--- app.js
+++ app.js
@@ -60,7 +60,10 @@ async function loadProducts() {
     "data/vlees-vis.json",
     "data/drank.json",
     "data/overig.json",
-    "data/zuivel-brood.json"
+    "data/zuivel-brood.json",
+    "data/vis.json",
+    "data/pasta.json",
+    "data/spices.json"
   ];
 
   try {
